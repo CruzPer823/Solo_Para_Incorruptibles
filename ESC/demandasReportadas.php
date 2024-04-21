2f5552ce85cded928c8b89abbf5260d0fa361178
Javier Cuatepotzo <javiercuatepotzo@Javiers-MacBook-Pro.local>
2024-04-21 14:12:22 -0600

    Tabla denuncias Reportadas lista

--- ESC/demandasReportadas.php
+++ ESC/demandasReportadas.php
@@ -66,18 +66,10 @@
             <tbody>
 
                 <?php
-
-            require './includes/database.php';
-        
-
-            
-        
+            require '../includes/dbh.inc.php';
    
             $consulta = "SELECT denuncia.id AS denuncia_id, estados.nombre AS nombre_estado, municipios.nombre AS nombre_municipio FROM denuncia INNER JOIN municipios ON denuncia.ubicacion = municipios.id INNER JOIN estados ON municipios.estado = estados.id;";
-            
-            if ($resultado = mysqli_query($mysqli, $consulta)) {
-  
-                while ($fila = mysqli_fetch_array($resultado)) {
+            foreach($pdo->query($consulta) as $fila){
                     echo "<tr>";
                     echo "<td><a href='detallesDenuncia.php?id=" . $fila['denuncia_id'] . "'>" . $fila['denuncia_id'] . "</a></td>";
                     echo "<td>" . $fila['nombre_estado'] . "</td>";
@@ -85,14 +77,7 @@
                     echo "<td> <input type='button' name='pub' class='boton-tabla boton--secundario no-margin' value='Reportar' onclick='confirmarReporte(" . $fila['denuncia_id'] . ")'></td>";
                     echo "<td> <input type='button' name='pub' class='boton-tabla boton--eliminar no-margin' value='Eliminar' onclick='eliminarRegistro(" . $fila['denuncia_id'] . ")'></td>";
                     echo "</tr>";
-                }
-       
-                mysqli_free_result($resultado);
-            } else {
-                echo "ERROR: No se pudo ejecutar $consulta. " . mysqli_error($mysqli);
             }
-        
-            mysqli_close($mysqli);
             ?>
             </tbody>
         </table>
--- includes/form_contr.inc.php
+++ includes/form_contr.inc.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 function UploadData(object $pdo, array $datos_iniciales, array $explicacion_info, array $ubicacion_info, array $sospechoso_info , array $time_info , array $conductas){
     $id = $datos_iniciales ["seccion_electoral"].$ubicacion_info ["municipio"];
     uploadDatosD($pdo, $id ,$datos_iniciales, $explicacion_info, $ubicacion_info, $sospechoso_info,$time_info);
-    // if(uploadConductas($pdo, $conductas['c1'], $id)){
-    //     return true;
-    // }else{
-    //     return false;
-    // }
+    uploadConductas($pdo, $conductas['c1'], $id);
+    uploadConductas($pdo, $conductas['c2'], $id);
+    uploadConductas($pdo, $conductas['c3'], $id);
+    uploadConductas($pdo, $conductas['c4'], $id);
+    uploadConductas($pdo, $conductas['c5'], $id);
+    uploadConductas($pdo, $conductas['c6'], $id);
+    uploadConductas($pdo, $conductas['c7'], $id);
+    uploadConductas($pdo, $conductas['c8'], $id);
+    echo'ok';    
 }
